5f201992677027723274657b54b0485997c21bd6
432player <432player@gmail.com>
2016-06-23 14:14:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', function(request, response) {
   response.send(item)
 })
 
-Parse.initialize(process.env.APPLICATION_ID, "",process.env.MASTER_KEY);
+Parse.initialize(process.env.APPLICATION_ID, process.env.MASTER_KEY);
 console.log("MY LOG" + Parse);
  
  console.log("APPID " + process.env.APPLICATION_ID)
