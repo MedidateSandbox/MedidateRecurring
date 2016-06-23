92fac9d4fba44c7496ec961b3ac4ccfdb12ab9e8
432player <432player@gmail.com>
2016-06-23 14:06:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ setInterval(function() {
 					   console.log("Sent" + error)
 					  },
 					  error: function(error) {
-					  	console.log("Error" + error.code)
+					  	console.log("Error" + error.message)
 					  }
 					});
    }, 10000)
