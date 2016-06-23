a118a79f9487915a1883181cbfe4e897ac4aa6fc
432player <432player@gmail.com>
2016-06-23 11:37:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.get('/', function(request, response) {
   response.send(item)
 })
 
+setInterval(function() { func(console.log("Goodbye")) }, 5000)
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
