3e6a901f56a648a9a6b5253303fddbb9c305b930
MedidateSandbox <33726864+MedidateSandbox@users.noreply.github.com>
2017-12-14 17:14:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,27 +9,36 @@ app.use(express.static(__dirname + '/public'))
 
 
 app.get('/', function(request, response) {
-  var items = ["a","e","i","o","u"]
-  var item = items[Math.floor(Math.random()*items.length)];
-  response.send(item)
+    var items = ["a", "e", "i", "o", "u"]
+    var item = items[Math.floor(Math.random() * items.length)];
+    response.send(item)
 })
 
-Parse.initialize(process.env.APPLICATION_ID,"", process.env.MASTER_KEY);
+Parse.initialize(process.env.APPLICATION_ID, "", process.env.MASTER_KEY);
 Parse.serverURL = process.env.PUBLIC_SERVER_URL;
 // Parse.Cloud.useMasterKey();
 
-setInterval(function() { 
-  Parse.Cloud.run('refreshRecurringSessions', {}, {
-	  				  useMasterKey: true,
-					  success: function(success) {
-					   console.log("Sent")
-					  },
-					  error: function(error) {
-					  	console.log("Error " + error.message)
-					  }
-					});
-   },60000 )//300000 5 * 60000)
+setInterval(function() {
+    Parse.Cloud.run('refreshRecurringSessions', {}, {
+        useMasterKey: true,
+        success: function(success) {
+            console.log("Started Reoccurring Method")
+            Parse.Cloud.run('sendAlertToSessionSubscribers', {}, {
+                useMasterKey: true,
+                success: function(success) {
+                    console.log("Started Alerts Method")
+                },
+                error: function(error) {
+                    console.log("Error " + error.message)
+                }
+            });
+        },
+        error: function(error) {
+            console.log("Error " + error.message)
+        }
+    });
+}, 60000) //300000 5 * 60000)
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+    console.log("Node app is running at localhost:" + app.get('port'))
 })
